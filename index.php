7eb93cd406ebdf9554a4b05c3dd6054085b155d1
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-20 23:12:30 +0100

    CSRF Protection and Sessions class added. You can now flash success/fail messages to the user using Session class

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 require_once 'core/init.php';
 
-$user = DB::getInstance()->get('users', array('username', '=', 'brendan'));
-
-if ($user->error()) {
-    echo 'No user';
-} else {
-    foreach ($user->results() as $user){
-        echo $user->username;
-    }
+if (Session::exists('success')) {
+    echo Session::flash('success');
 }
